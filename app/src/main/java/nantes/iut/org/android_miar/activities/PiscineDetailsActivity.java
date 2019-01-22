5e1efbf99a19dff453df8ae1485ba239fff1f47b
veax <borissowadim@mail.ru>
2019-01-22 22:33:18 +0100

    toggle button shared preferences saving state

--- app/src/main/java/nantes/iut/org/android_miar/activities/PiscineDetailsActivity.java
+++ app/src/main/java/nantes/iut/org/android_miar/activities/PiscineDetailsActivity.java
@@ -1,12 +1,15 @@
 package nantes.iut.org.android_miar.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import nantes.iut.org.android_miar.R;
 import nantes.iut.org.android_miar.entities.Piscine;
+import nantes.iut.org.android_miar.listeners.IsVisitedButtonListener;
 
 public class PiscineDetailsActivity extends AppCompatActivity {
 
@@ -14,14 +17,32 @@ public class PiscineDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.piscine_details);
-        //Bundle data = getIntent().getExtras();// get extras from MainActivity
         Intent intent = getIntent();
         Piscine piscine = (Piscine) intent.getParcelableExtra("piscine");  // get Piscine object;
 
         TextView textNom = (TextView) findViewById(R.id.nom_piscine);
         textNom.setText(piscine.getNom_complet());
 
-        // set listener on stars evaluation
+        TextView textTelephone = (TextView) findViewById(R.id.num_tel);
+        textTelephone.setText(piscine.getTel());
+
+        TextView textBassinSportif = (TextView) findViewById(R.id.textViewBassinSportif);
+        textBassinSportif.setText(piscine.getBassin_sportif());
+        TextView textViewToboggan= (TextView) findViewById(R.id.textViewToboggan);
+        textViewToboggan.setText(piscine.getToboggan());
+        TextView textViewAccesHandicap = (TextView) findViewById(R.id.textViewAccesHadicap);
+        textViewAccesHandicap.setText(piscine.getAccessibilite_handicap());
+        TextView textViewBassinLoisir= (TextView) findViewById(R.id.textViewBassinLoisir);
+        textViewBassinLoisir.setText(piscine.getBassin_loisir());
+
+
+        ToggleButton isVisited_btn = (ToggleButton) findViewById(R.id.isVisited_btn);
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        boolean tgpref = preferences.getBoolean("tgpref", false);  //default is true
+        isVisited_btn.setChecked(tgpref);
         // set listener on "visited" button
+        isVisited_btn.setOnClickListener(new IsVisitedButtonListener(isVisited_btn, this));
+
+        // set listener on stars evaluation
     }
 }
---
+++ app/src/main/java/nantes/iut/org/android_miar/listeners/IsVisitedButtonListener.java
@@ -0,0 +1,29 @@
+package nantes.iut.org.android_miar.listeners;
+
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.ToggleButton;
+
+import nantes.iut.org.android_miar.activities.PiscineDetailsActivity;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class IsVisitedButtonListener implements View.OnClickListener {
+    private ToggleButton isVisited;
+    private PiscineDetailsActivity piscineDetailsActivity;
+
+    public IsVisitedButtonListener(ToggleButton isVisited, PiscineDetailsActivity piscineDetailsActivity) {
+        this.isVisited = isVisited;
+        this.piscineDetailsActivity = piscineDetailsActivity;
+    }
+
+    @Override
+    public void onClick(View v) {
+        boolean checked = isVisited.isChecked();
+        System.out.println(checked);
+        SharedPreferences pref = piscineDetailsActivity.getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("tgpref", checked); // value to store
+        editor.commit();
+    }
+}
