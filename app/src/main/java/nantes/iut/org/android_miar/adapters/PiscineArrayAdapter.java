cf7fc9d15bc9ec33854411738adcecbe3815904e
veax <borissowadim@mail.ru>
2019-01-23 17:45:46 +0100

    add visited text on main activity

--- app/src/main/java/nantes/iut/org/android_miar/adapters/PiscineArrayAdapter.java
+++ app/src/main/java/nantes/iut/org/android_miar/adapters/PiscineArrayAdapter.java
@@ -2,6 +2,7 @@ package nantes.iut.org.android_miar.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,10 @@ import android.widget.TextView;
 import java.util.List;
 
 import nantes.iut.org.android_miar.R;
+import nantes.iut.org.android_miar.activities.MainActivity;
 import nantes.iut.org.android_miar.entities.Piscine;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class PiscineArrayAdapter extends ArrayAdapter<Piscine> {
 
@@ -35,14 +39,20 @@ public class PiscineArrayAdapter extends ArrayAdapter<Piscine> {
         TextView textBassinSportif = view.findViewById(R.id.textViewBassinSportif);
         TextView textBassinLoisir = view.findViewById(R.id.textViewBassinLoisir);
         TextView textAccesHandicap = view.findViewById(R.id.textViewAccesHadicap);
-        TextView textIsOpen = view.findViewById(R.id.textViewOpen);
+        TextView textViewVisited = view.findViewById(R.id.textViewVisited);
 
         textNom.setText(getItem(position).getNom_usuel());
         textAdresse.setText(getItem(position).getAdresse());
         textBassinLoisir.setText(getItem(position).getBassin_loisir());
         textBassinSportif.setText(getItem(position).getBassin_sportif());
         textAccesHandicap.setText(getItem(position).getAccessibilite_handicap());
-        textIsOpen.setText(boolean_toStringOpen(getItem(position).isOpen()));
+
+        SharedPreferences preferences = ((Activity) context).getPreferences(MODE_PRIVATE);
+        String visite = "NON";
+        if (preferences.getBoolean(getItem(position).getRecordid() + "tgpref", true) == true) {
+            visite = "OUI";
+        }
+        textViewVisited.setText(visite);
 
         return view;
     }
